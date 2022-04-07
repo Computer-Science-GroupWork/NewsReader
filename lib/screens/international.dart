0ef1059a2ea1daedcc25e00c7818b84cd158cf49
maqamylee0 <maqamylee0@gmail.com>
2022-04-07 23:11:45 +0300

    local news

---
+++ lib/screens/international.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class International extends StatefulWidget {
+  const International({Key? key}) : super(key: key);
+
+  @override
+  State<International> createState() => _InternationalState();
+}
+
+class _InternationalState extends State<International> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/theme.dart
@@ -0,0 +1,41 @@
+
+import 'package:flutter/material.dart';
+
+import 'constants.dart';
+
+CustomTheme currentTheme = CustomTheme();
+
+
+
+class CustomTheme with ChangeNotifier {
+  static bool _isDarkTheme = true;
+  ThemeMode get currentTheme=> _isDarkTheme ? ThemeMode.dark:ThemeMode.light;
+
+  void toggleTheme(){
+    _isDarkTheme =!_isDarkTheme;
+    notifyListeners();
+  }
+  static ThemeData get lightTheme { //1
+    return ThemeData( //2
+        primaryColor: fPrimaryColor,
+        scaffoldBackgroundColor: fBackgroundColor,
+        fontFamily: 'NunitoSans', //3
+        buttonTheme: ButtonThemeData( // 4
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18.0)),
+          buttonColor: fButtonColor,
+        )
+    );
+  }
+  static ThemeData get darkTheme {
+    return ThemeData(
+        primaryColor: Colors.grey,
+        scaffoldBackgroundColor: Colors.black,
+        fontFamily: 'NunitoSans',
+        textTheme: ThemeData.dark().textTheme,
+        buttonTheme: ButtonThemeData(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18.0)),
+          buttonColor: fButtonColor,
+        )
+    );
+  }
+}
